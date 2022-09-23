3adc6110afc7bd7d966fd7da01b6852b6298f1d1
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-09-23 23:12:43 +0100

    first commit

---
+++ OsamasProject/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+int main(){
+    int myAge;
+    printf("enter your age:\n");
+    scanf("%d", &myAge); 
+    int girlsAge = (myAge / 2) + 7;
+    printf("you can date %d or older \n", girlsAge);
+    return 0;
+}
